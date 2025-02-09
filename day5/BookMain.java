1693536505e1bc2317f75e251bf5ddcd7be1d494
POOJARI ROHITH KUMAR <poojarirohithkumar2003@gmail.com>
2025-02-09 15:47:17 +0530

    day5 commit

--- day5/BookMain.java
+++ day5/BookMain.java
@@ -1,83 +1,65 @@
 package com.celcom.day5;
- 
- 
+
 import java.util.ArrayList;
- 
-class Book {
-     String title;
-     String author;
-     String isbn;
- 
-    
-    public Book(String title, String author, String isbn) {
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-    }
- 
-    
-    public String getTitle() {
-        return title;
-    }
- 
-    public String getAuthor() {
-        return author;
-    }
- 
-    public String getIsbn() {
-        return isbn;
-    }
- 
-    
-    public String write() {
-        return "Title: " + title + "Author: " + author + "ISBN: " + isbn ;
-    }
+
+class Book{
+	String title;
+	String author;
+	String isbn;
+	
+	Book(String title, String author, String isbn){
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+	}
+	
+	String write() {
+		return "title : " + title + "author : " + author + "ISBN : " + isbn;
+	}
+	
 }
- 
+
 class Library {
-    ArrayList<Book> collection= new ArrayList<>();
- 
-    
-    public void addBook(Book book) {
-    	collection.add(book);
-        System.out.println("Book added : " + book.write());
-    }
- 
-    public void removeBook(String isbn) {
-        for (Book book : collection) {
-            if (book.getIsbn().equals(isbn)) {
-            	collection.remove(book);
-                System.out.println("Book removed : " + book.write());
-                return;
-            }
-        }
-        System.out.println("Book with ISBN " + isbn + " not found.");
-      
-    }    
-    public void displayBooks() {
-            System.out.println("\nBooks in the collection:");
-            for (Book book : collection) {
-                System.out.println(book.write());
-            }
-    }
+	ArrayList<Book> books = new ArrayList<>();
+	
+	void addBooks(String name, String author, String isbn) {
+		Book newBook = new Book(name, author, isbn);
+		books.add(newBook);
+	}
+	
+	void removeBook(String isbn) {
+		for(Book book: books) {
+			if(book.isbn.equals(isbn)) {
+				books.remove(book);
+				System.out.println("The book removed: \n" + book.write());
+				return;
+			}
+		}
+		System.out.println("The book with ISBN " + isbn + " not found");
+	}
+	
+	void displayBooks() {
+		System.out.println("\nThe books are available in Library : ");
+		for(Book book : books) {
+			System.out.println(book.write());
+		}
+		
+		System.out.println("------------------------------------\n");
+	}
 }
- 
 public class BookMain {
-    public static void main(String[] args) {
-        Library library = new Library();
- 
-        
-        Book book1 = new Book("Book1 ", "Author1 ", "123");
-        Book book2 = new Book("Book2 ", "Author2 ", "456");
- 
-        
-        library.addBook(book1);
-        library.addBook(book2);
- 
-        library.removeBook("123");
-        library.displayBooks();
-        
-        
-    }
+	public static void main(String[] args) {
+		Library library = new Library();
+		library.addBooks("Book1", "A", "123");
+		library.addBooks("Book2", "B", "456");
+		library.addBooks("Book3", "C", "789");
+		
+		library.displayBooks();
+		
+		library.removeBook("456");
+		
+		library.displayBooks();
+		
+	}
+
 }
- 
