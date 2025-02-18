97e96da254cfb4a287d5ab9012b9506d5f9590e9
POOJARI ROHITH KUMAR <poojarirohithkumar2003@gmail.com>
2025-02-18 09:56:32 +0530

    monday assignment

--- Monday-Assignments[17-Feb-2025]/Assignment5.java
+++ Monday-Assignments[17-Feb-2025]/Assignment5.java
@@ -2,29 +2,30 @@ package com.celcom.day10;
 
 import java.util.Scanner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Assignment5 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the size of aray : ");
-		int n = sc.nextInt();
-		int arr[] = new int[n];
-		for(int i=0;i<n;i++) {
-			arr[i] = sc.nextInt();
-		}
-		
-		for(int i=0;i<n;i++) {
-			for(int j = i+1; j<n;j++) {
-				if(arr[i] > arr[j]) {
-					int temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
-				}
-			}
-		}
-		
-		for(int i=0;i<n;i++) {
-			System.out.print(arr[i]+" ");
-		}
-		
-	}
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<Integer>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array :");
+        int n = sc.nextInt();
+        System.out.println("Eneter elements: ");
+        for(int i=0;i<n;i++) {
+        	numbers.add(sc.nextInt());
+        }
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            for (int j = 0; j < numbers.size() - i - 1; j++) {
+                if (numbers.get(j) > numbers.get(j + 1)) {
+                    int temp = numbers.get(j);
+                    numbers.set(j, numbers.get(j + 1));
+                    numbers.set(j + 1, temp);
+                }
+            }
+        }
+ 
+        System.out.println("Sorted ArrayList: " + numbers);
+    }
 }
+ 
